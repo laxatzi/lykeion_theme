635add3805a799e874049b82b1e4c4859962dba5
laxatzi <laxatzi@yahoo.com>
2024-04-01 16:39:34 +0300

    Add stylesheet to functions

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
   function edutheme_files() {
     wp_enqueue_style('edutheme_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
   }
 
   add_action('wp_enqueue_scripts', 'edutheme_files');
