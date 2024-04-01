19b8aaf832996ecccb8f30d8dc8eba5b76da920d
laxatzi <laxatzi@yahoo.com>
2024-04-01 17:43:28 +0300

    add external styles to functions

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
   function edutheme_files() {
-    wp_enqueue_style('edutheme_main_styles', get_stylesheet_uri('/css/style.css'));
+    wp_enqueue_style('edutheme_google_font_montserrat', '//fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+    wp_enqueue_style('edutheme_bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+    wp_enqueue_style('edutheme_main_styles', get_theme_file_uri('/css/index.css'));
     wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
   }
 
