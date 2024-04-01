dce65b7fdbbc7256c3f48eaa55e156989e5e0734
laxatzi <laxatzi@yahoo.com>
2024-04-01 17:06:32 +0300

    add enqueue style sheet

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
   function edutheme_files() {
-    wp_enqueue_style('edutheme_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('edutheme_main_styles', get_stylesheet_uri('/css/style.css'));
     wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
   }
 
