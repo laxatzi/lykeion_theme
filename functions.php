b9432d42252e76d1ca6dcc8f3c93b98226766300
laxatzi <laxatzi@yahoo.com>
2024-04-05 16:46:37 +0300

    add script file to functions

--- functions.php
+++ functions.php
@@ -5,42 +5,8 @@
     wp_enqueue_style('edutheme_bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('edutheme_main_styles', get_theme_file_uri('/css/index.css'));
     wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
+    wp_enqueue_script('main js file', get_theme_file_uri("/js/index.js"), array('jquery'), '1.0.0', true );
   }
 
   add_action('wp_enqueue_scripts', 'edutheme_files');
 
-
-   function wpb_hook_javascript_footer() {
-?>
-<script>
-
-  const scrollTopButton = document.getElementById("js--back-to-top");
-
-      // When the user scrolls down 350px from the top of the document, show the button
-      window.onscroll = function () {
-        scrollFunction();
-      };
-
-      function scrollFunction() {
-        if (
-          document.body.scrollTop > 350 ||
-          document.documentElement.scrollTop > 350
-        ) {
-          scrollTopButton.style.opacity = 1;
-        } else {
-          scrollTopButton.style.opacity = 0;
-        }
-      }
-
-      // When the user clicks on the button, scroll to the top of the document
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-
-      scrollTopButton.addEventListener("click", topFunction)
-
-</script>
-<?php
-   }
-add_action('wp_footer', 'wpb_hook_javascript_footer');
