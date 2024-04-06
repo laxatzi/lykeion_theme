a1f2f8a656846932eb8edba3e521c566d7c982e2
laxatzi <laxatzi@yahoo.com>
2024-04-06 12:48:42 +0300

    add wp functions in header

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
   <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-     <?php wp_head(); ?>
+    <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?>>
     <a class="back-to-top hidden" href="#" id="js--back-to-top" title="Go to top">
       <span class="vicon-back-to-top"></span>
       <span class="sr-only">Back to top</span>
