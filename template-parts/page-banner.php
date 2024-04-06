b92e7b512c0647b43bd93a914de566194e73c287
laxatzi <laxatzi@yahoo.com>
2024-04-06 17:40:21 +0300

    Add template in template parts

---
+++ template-parts/page-banner.php
@@ -0,0 +1,41 @@
+      <section id="hero" class="container pt-5 pb-5">
+        <div class="intro pt-sm-5 pt-4 pb-3 px-sm-2 px-3">
+          <h1 class="mt-5 mb-1 fw-bold">About Us</h1>
+          <nav class="breadcrumb-nav">
+            <ol class="breadcrumb d-flex">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="separator">
+                <span class="vicon-double-right-arrow-black"></span>
+              </li>
+              <li class="breadcrumb-item" aria-current="page">About</li>
+            </ol>
+          </nav>
+        </div>
+      </section>
+      <!-- Shapes -->
+      <div class="shapes">
+        <div class="shape-image shape-image-32">
+          <img
+            class="img-fluid"
+            src="img/icons/world_orange-small.png"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-33">
+          <img
+            class="img-fluid"
+            src="img/shapes/Lykeion/shape-circles.png"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-34">
+          <img
+            class="img-fluid"
+            src="img/shapes/Lykeion/geo-shape-transparent-15.png"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-35">
+          <img class="img-fluid" src="img/icons/abstract419.png" alt="" />
+        </div>
+      </div>
