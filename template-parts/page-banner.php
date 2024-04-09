fa5f8a0b6d3ccca45d572b717d70423a30f20737
laxatzi <laxatzi@yahoo.com>
2024-04-09 12:20:59 +0300

    change template links and breadcrumb position in styles

--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -1,13 +1,13 @@
       <section id="hero" class="container pt-5 pb-5">
         <div class="intro pt-sm-5 pt-4 pb-3 px-sm-2 px-3">
-          <h1 class="mt-5 mb-1 fw-bold">About Us</h1>
+          <h1 class="mt-5 mb-1 fw-bold"><?php echo $pagename ?></h1>
           <nav class="breadcrumb-nav">
             <ol class="breadcrumb d-flex">
-              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item"><a href="<?php echo site_url() ?>" ?>Home</a></li>
               <li class="separator">
                 <span class="vicon-double-right-arrow-black"></span>
               </li>
-              <li class="breadcrumb-item" aria-current="page">About</li>
+              <li class="breadcrumb-item" aria-current="page"><?php echo $pagename ?></li>
             </ol>
           </nav>
         </div>
