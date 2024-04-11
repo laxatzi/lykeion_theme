bd15f7e7719f8f7049f995a75c9aa97a3470e06b
laxatzi <laxatzi@yahoo.com>
2024-04-11 14:46:30 +0300

    add code for meta"

--- single.php
+++ single.php
@@ -10,23 +10,28 @@
         <div class="row">
           <div class="col-lg-10 col-md-10" id="blog-details-section">
             <div class="blog-details--meta d-xl-flex mb-xl-5 mb-sm-4 mb-3 justify-content-between">
-              <div class="tag text-uppercase"><a href="tag-page.html">Business</a></div>
+              <div class="tag text-uppercase"><a href="tag-page.html"><?php
+                	foreach((get_the_category()) as $category){
+		               echo $category->name;
+                  }
+              ?></a></div>
                 <div class="meta d-xl-flex justify-content-between">
                   <div class="author-meta mb-2 d-flex align-items-center">
                     <img src="img/usman-thumb.jpg" alt="">
-                    <h5 class="ms-2 mt-1">John Katsimatides</h5>
+                    <h5 class="ms-2 mt-1"><?php echo get_the_author_meta('nickname'); ?> </h5>
                   </div>
                   <div class="blogPost-date-meta d-flex align-items-center">
                     <span class="vicon-calendar"></span>
-                    <h5 class="ms-2">Nov 12, 2023</h5>
+                    <h5 style="font-style:italic" class="ms-2"><?php $publish_date = get_the_time('j F, Y');
+                   echo $publish_date; ?></h5>
                   </div>
                   <div class="comments-meta d-flex align-items-center">
                     <span class="vicon-comment"></span>
-                    <h5 class="ms-2">4 Comments</h5>
+                    <h5 class="ms-2"> <?php echo $post->comment_count ?></h5>
                   </div>
                   <div class="time-to-read d-flex align-items-center">
                     <span class="vicon-time"></span>
-                    <h5 class="ms-2">6 min read</h5>
+                    <h5 class="ms-2"><?php echo do_shortcode('[rt_reading_time]') ?> min read</h5>
                   </div>
               <!-- end meta -->
               </div>
