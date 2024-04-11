d5f6e6871659e305717c55fc31ac3e39ed13cb43
laxatzi <laxatzi@yahoo.com>
2024-04-11 12:29:59 +0300

    add display popular posts function

--- functions.php
+++ functions.php
@@ -69,3 +69,30 @@ add_filter( 'manage_posts_columns', 'gt_posts_column_views' );
 add_action( 'manage_posts_custom_column', 'gt_posts_custom_column_views' );
 
 
+
+// Display Popular Posts
+function wp_display_popular_posts($num_posts) {
+
+ $popular = new WP_Query(array(
+     'posts_per_page' => $num_posts,
+     'meta_key' => 'post_views_count',
+     'orderby' => 'meta_value_num',
+     'order' => 'DESC'
+ ));
+
+ if ($popular->have_posts()) {
+     while ($popular->have_posts()) {
+         $popular->the_post();
+         echo '<div class="latest-post-item d-flex py-4">
+                <div class="latest-post-info">
+                  <div class="latest-post-date">
+                   <small style="font-style:italic">'.$publish_date = get_the_time('j F, Y');
+                   echo $publish_date.'</small>
+                  </div><div class="latest-post-title"><h6 class="fw-bold"><a href="'.get_permalink().'">'.get_the_title().'</a></h6></div></div></div>';
+     }
+ }
+
+ wp_reset_postdata();
+}
+
+
