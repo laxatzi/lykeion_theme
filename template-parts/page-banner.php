995481f60d46d2ba5a5d56efb4b5196439033f1d
laxatzi <laxatzi@yahoo.com>
2024-04-12 12:09:09 +0300

    change style and layout to header templates

--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -1,5 +1,5 @@
-      <section id="hero" class="container pt-5 pb-5">
-        <div class="intro pt-sm-5 pt-4 pb-3 px-sm-2 px-3">
+     <section id="hero" class="container-fluid pt-5 pb-5">
+        <div class="intro container pt-sm-5 pt-4 pb-3 px-sm-2 px-3">
           <h1 class="mt-5 mb-1 fw-bold"><?php echo $pagename ?></h1>
           <nav class="breadcrumb-nav">
             <ol class="breadcrumb d-flex">
