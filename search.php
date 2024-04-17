a0d6328c570724fbb7cbea0ae7c2423eb8e826d2
laxatzi <laxatzi@yahoo.com>
2024-04-17 17:17:10 +0300

    Introduce template part depending on post type

--- search.php
+++ search.php
@@ -39,22 +39,9 @@
           <?php
             while(have_posts()) {
               the_post();
+              get_template_part('template-parts/content', get_post_type());
             ?>
-            <div class="the-blogPost py-4"  >
-            <!-- title -->
-           <div class="blogPost-title px-4 mb-4">
-            <a href="<?php the_permalink(); ?>">
-              <h2 class="fw-bold"><?php the_title(); ?></h2>
-            </a>
-           </div>
-           <div class="blogPost-content px-4 pb-4">
-             <?php the_excerpt(); ?>
-              <div class="read-more mt-3 mb-3 btn btn-lg col-md-4 col-sm-12">
-                <a href="<?php the_permalink(); ?>" class="link-btn">Read More</a>
-                <span class="vicon-arrow--right"></span>
-              </div>
-            </div>
-          </div>
+
           <?php
             }
           ?>
