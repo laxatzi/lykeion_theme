8bacafdeae0ba78f35f5469750482d2548decf54
laxatzi <laxatzi@yahoo.com>
2024-04-17 17:42:43 +0300

    Remove navigation

--- template-parts/banners/search-banner.php
+++ template-parts/banners/search-banner.php
@@ -3,10 +3,6 @@
           <h2 class="banner mt-5 mb-1 fw-bold"><a href="<?php echo site_url('/blog') ?>">Search Results</a></h2>
           <nav class="breadcrumb-nav">
             <ol class="breadcrumb d-flex">
-              <li class="breadcrumb-item"><a href="<?php echo site_url() ?>" ?>Home</a></li>
-              <li class="separator">
-                <span class="vicon-double-right-arrow-black"></span>
-              </li>
               <li class="breadcrumb-item" aria-current="page"><?php echo 'You searched for &ldquo;<i class="search-query">' . esc_html(get_search_query()) . '</i>&rdquo;' ?></li>
             </ol>
           </nav>
