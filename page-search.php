ac0183f8ce0df39f7444b0d2dd9ad11c5584cfdf
laxatzi <laxatzi@yahoo.com>
2024-04-17 12:56:14 +0300

    Style search input

--- page-search.php
+++ page-search.php
@@ -11,11 +11,14 @@
         <div class="container py-sm-2">
           <div class="row mt-sm-5">
             <h2><?php the_title(); ?></h2>
-             <form method="get" action="<?php echo esc_url(site_url('')) ?>">
-              <input type="search" name="s">
-              <input type="submit" value="Search">
-
-             </form>
+             <div class="input-group mb-3 d-flex align-items-center">
+                <form class="search-form" method="get" action="<?php echo esc_url(site_url('')) ?>">
+                  <input type="search" name="s" class="form-control p-2" aria-label="Search keyword">
+                  <div class="input-group-append">
+                    <input class="input-group-search p-2 fw-bold" type="submit" value="Search">
+                  </div>
+                </form>
+              </div>
           </div>
        </div>
      </div>
