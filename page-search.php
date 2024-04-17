699d41dd3a4707b6e5b3dde167271985aa5fa7fa
laxatzi <laxatzi@yahoo.com>
2024-04-17 12:30:38 +0300

    Add search input form'

--- page-search.php
+++ page-search.php
@@ -1,13 +1,26 @@
 <?php
 
     get_header('inner');
-    get_template_part('template-parts/page-banner');
+    get_template_part('template-parts/post-banner');
 
 
     while(have_posts()) {
     the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+ <main class="container-fluid">
+      <div class="p-5" id="about-us">
+        <div class="container py-sm-2">
+          <div class="row mt-sm-5">
+            <h2><?php the_title(); ?></h2>
+             <form method="get" action="<?php echo esc_url(site_url('')) ?>">
+              <input type="search" name="s">
+              <input type="submit" value="Search">
+
+             </form>
+          </div>
+       </div>
+     </div>
+  </main>
+
   <?php
     }
 
