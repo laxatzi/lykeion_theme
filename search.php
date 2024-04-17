58bd3eae3a002de18c0661f0d4b186539dd72a89
laxatzi <laxatzi@yahoo.com>
2024-04-17 17:27:28 +0300

    troubleshoot search post type

--- search.php
+++ search.php
@@ -39,7 +39,7 @@
           <?php
             while(have_posts()) {
               the_post();
-              get_template_part('template-parts/content', get_post_type());
+              get_template_part('template-parts/content/content', get_post_type());
             ?>
 
           <?php
