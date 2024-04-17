5856d986a8a2ed1252499bfb0d2af283b7158fb7
laxatzi <laxatzi@yahoo.com>
2024-04-17 16:01:27 +0300

    add placeholder

--- page-search.php
+++ page-search.php
@@ -1,7 +1,7 @@
 <?php
 
     get_header('inner');
-    get_template_part('template-parts/post-banner');
+    get_template_part('template-parts/page-banner');
 
 
     while(have_posts()) {
@@ -13,7 +13,7 @@
             <h2><?php the_title(); ?></h2>
              <div class="input-group mb-3 d-flex align-items-center">
                 <form class="search-form" method="get" action="<?php echo esc_url(site_url('')) ?>">
-                  <input type="search" name="s" class="form-control p-2" aria-label="Search keyword">
+                  <input type="search" name="s" class="form-control p-2" aria-label="Search keyword" placeholder="What are you looking for?">
                   <div class="input-group-append">
                     <input class="input-group-search p-2 fw-bold" type="submit" value="Search">
                   </div>
