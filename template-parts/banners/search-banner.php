4cefef59f2e7a7cac8077b1f7b4ece77282f0b4f
laxatzi <laxatzi@yahoo.com>
2024-04-17 16:58:13 +0300

    Init template-parts folder banner and move all banners into it

---
+++ template-parts/banners/search-banner.php
@@ -0,0 +1,44 @@
+     <section id="hero" class="container-fluid pt-5 pb-5">
+        <div class="intro container pt-sm-5 pt-4 pb-3 px-sm-2 px-3">
+          <h2 class="banner mt-5 mb-1 fw-bold"><a href="<?php echo site_url('/blog') ?>">Search Results</a></h2>
+          <nav class="breadcrumb-nav">
+            <ol class="breadcrumb d-flex">
+              <li class="breadcrumb-item"><a href="<?php echo site_url() ?>" ?>Home</a></li>
+              <li class="separator">
+                <span class="vicon-double-right-arrow-black"></span>
+              </li>
+              <li class="breadcrumb-item" aria-current="page"><?php echo 'You searched for &ldquo;<i class="search-query">' . esc_html(get_search_query()) . '</i>&rdquo;' ?></li>
+            </ol>
+          </nav>
+        </div>
+      </section>
+      <!-- Shapes -->
+      <div class="shapes">
+        <div class="shape-image shape-image-32">
+          <img
+            class="img-fluid"
+            src="<?php echo get_theme_file_uri('/img/icons/world_orange-small.png') ?>"
+            src="img/icons/world_orange-small.png"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-33">
+          <img
+            class="img-fluid"
+            src="<?php echo get_theme_file_uri('/img/shapes/shape-circles.png') ?>"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-34">
+          <img
+            class="img-fluid"
+            src="<?php echo get_theme_file_uri('/img/shapes/geo-shape-transparent-15.png') ?>"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-35">
+          <img class="img-fluid"
+          src="<?php echo get_theme_file_uri('/img/icons/abstract419.png') ?>"
+          alt="" />
+        </div>
+      </div>
