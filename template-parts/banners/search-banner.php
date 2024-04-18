d26fc24aa43ab234fcae24c27ca0530d9b7e18a4
laxatzi <laxatzi@yahoo.com>
2024-04-18 13:39:55 +0300

    remove link form heading

--- template-parts/banners/search-banner.php
+++ template-parts/banners/search-banner.php
@@ -1,6 +1,6 @@
      <section id="hero" class="container-fluid pt-5 pb-5">
         <div class="intro container pt-sm-5 pt-4 pb-3 px-sm-2 px-3">
-          <h2 class="banner mt-5 mb-1 fw-bold"><a href="<?php echo site_url('/blog') ?>">Search Results</a></h2>
+          <h2 class="banner mt-5 mb-1 fw-bold">Search Results</h2>
           <nav class="breadcrumb-nav">
             <ol class="breadcrumb d-flex">
               <li class="breadcrumb-item" aria-current="page"><?php echo 'You searched for &ldquo;<i class="search-query">' . esc_html(get_search_query()) . '</i>&rdquo;' ?></li>
