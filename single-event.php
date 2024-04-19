765ef1be3b5d882a04869d4aeda4a0c94e692b50
laxatzi <laxatzi@yahoo.com>
2024-04-19 17:35:16 +0300

    clean code

--- single-event.php
+++ single-event.php
@@ -20,119 +20,116 @@
                   <h1 class="fw-bold mt-4 mb-5"><?php the_title();?></h1>
               </div>
           <!-- main event content -->
-            <div id="intro-page">
-              <div class="main-event-content" id="intro">
-                <?php
-                  the_content();
-                ?>
+              <div id="intro-page">
+                <div class="main-event-content" id="intro">
+                  <?php
+                    the_content();
+                  ?>
+                </div>
               </div>
-            </div>
-      </main>
-      <aside class="d-flex flex-column event-sidebar">
-            <div class="event-title">
-              <h2 class="text-center fw-bold py-5">Event Details</h2>
-            </div>
-            <div class="event-detailing p-3">
+            </main>
+            <aside class="d-flex flex-column event-sidebar">
+              <div class="event-title">
+                <h2 class="text-center fw-bold py-5">Event Details</h2>
+              </div>
+              <div class="event-detailing p-3">
               <!-- Event-date -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-calendar-event me-2"></span>
-                  <span>Event Date</span>
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-calendar-event me-2"></span>
+                    <span>Event Date</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
+                    <span>07/12/2023</span>
+                  </div>
                 </div>
-                <div class="info-list-duration fw-bold d-flex">
-
-                  <span>07/12/2023</span>
-                </div>
-              </div>
               <!-- event duration -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-duration me-2"></span>
-                  <span>Duration</span>
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-duration me-2"></span>
+                    <span>Duration</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
+                    <span>3h 00m</span>
+                  </div>
                 </div>
-                <div class="info-list-duration fw-bold d-flex">
-
-                  <span>3h 00m</span>
-                </div>
-              </div>
               <!-- enrolled users so far -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-users me-2"></span>
-                  <span>Enrolled So Far</span>
-                </div>
-                <div class="info-list-duration fw-bold d-flex">
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-users me-2"></span>
+                    <span>Enrolled So Far</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
 
-                  <span>72</span>
+                    <span>72</span>
+                  </div>
                 </div>
-              </div>
           <!-- Start Time -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-time-primary me-2"></span>
-                  <span>Start Time</span>
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-time-primary me-2"></span>
+                    <span>Start Time</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
+                    <span>10:00</span>
+                  </div>
                 </div>
-                <div class="info-list-duration fw-bold d-flex">
-
-                  <span>10:00</span>
-                </div>
-              </div>
             <!-- End Time -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-time-primary me-2"></span>
-                  <span>End Time</span>
-                </div>
-                <div class="info-list-duration fw-bold d-flex">
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-time-primary me-2"></span>
+                    <span>End Time</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
 
-                  <span>13:30</span>
+                    <span>13:30</span>
+                  </div>
                 </div>
-              </div>
             <!-- Location -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-geo-pin--primary me-2"></span>
-                  <span>Location</span>
-                </div>
-                <div class="info-list-duration fw-bold d-flex">
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-geo-pin--primary me-2"></span>
+                    <span>Location</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
 
-                  <span>City Center</span>
+                    <span>City Center</span>
+                  </div>
                 </div>
-              </div>
              <!-- Price -->
-              <div class="info-list d-flex mt-3 mb-3 p-3">
-                <div class="info-list-level fw-bold d-flex">
-                  <span class="vicon-tag--primary me-2"></span>
-                  <span>Price</span>
-                </div>
-                <div class="info-list-duration fw-bold d-flex">
+                <div class="info-list d-flex mt-3 mb-3 p-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <span class="vicon-tag--primary me-2"></span>
+                    <span>Price</span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
 
-                  <span>&euro; 29</span>
+                    <span>&euro; 29</span>
+                  </div>
                 </div>
-              </div>
             <!-- book seat -->
-            <div class="event-action">
-              <div class="book-seat btn btn-lg mt-5 col-11">
-                <a href="" class="link-btn">Book your seat</a>
-                <span class="vicon-arrow--right"></span>
-              </div>
-              <div class="share-event d-flex mt-5 align-items-start">
-                <h6 class="me-3">Share:</h6>
-                <a href=""><span class="vicon-x me-3"></span></a>
-                <a href=""><span class="vicon-facebook me-3"></span></a>
-                <a href=""><span class="vicon-youtube me-3"></span></a>
-                <a href=""><span class="vicon-linkedin "></span></a>
-              </div>
-             </div>
-            </div>
-      </aside>
+                <div class="event-action">
+                 <div class="book-seat btn btn-lg mt-5 col-11">
+                   <a href="" class="link-btn">Book your seat</a>
+                   <span class="vicon-arrow--right"></span>
+                 </div>
+                 <div class="share-event d-flex mt-5 align-items-start">
+                    <h6 class="me-3">Share:</h6>
+                    <a href=""><span class="vicon-x me-3"></span></a>
+                    <a href=""><span class="vicon-facebook me-3"></span></a>
+                    <a href=""><span class="vicon-youtube me-3"></span></a>
+                    <a href=""><span class="vicon-linkedin "></span></a>
+                  </div>
+                </div>
+               </div>
+            </aside>
 <!-- end of course-info-main section -->
-      </div>
+        </div>
 <!-- end row -->
-    </div>
+      </div>
 <!-- end of container -->
-  </div>
-</main>
+    </div>
+  </main>
   <?php
     }
 
