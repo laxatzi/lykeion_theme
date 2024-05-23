2742af59669a3d3ace01dfaa99f5aa84cf97403e
laxatzi <laxatzi@yahoo.com>
2024-05-23 17:37:08 +0300

    Refactor page layout

--- page.php
+++ page.php
@@ -6,8 +6,20 @@
 
     while(have_posts()) {
     the_post(); ?>
-    <h2><?php the_title(); ?></h2>
+
+
+    <main class="container-fluid">
+      <div class="py-5 pe-5 container" id="privacy-section">
+        <div class="row mt-5 mb-5">
+          <div class="col-lg-10">
+            <div class="privacy-rules">
+                  <h2 class="mb-5"><?php the_title(); ?></h2>
     <?php the_content(); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   <?php
     }
 
