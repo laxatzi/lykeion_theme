a16905c6400bce0721e02d4519730c385430d513
laxatzi <laxatzi@yahoo.com>
2024-05-29 14:44:14 +0300

    add masonry script in functions

--- functions.php
+++ functions.php
@@ -33,6 +33,12 @@
 
     }
 
+    if ( is_page('gallery') ) {
+
+    wp_enqueue_script('my-custom-script', get_stylesheet_directory_uri() . '/js/masonry.js', array(), false, true);
+
+    }
+
 }
 
 add_action('wp_enqueue_scripts', 'add_custom_js_to_specific_page');
