c96115d0e65234fafb892f92fd5beb0d6f5c2cca
laxatzi <laxatzi@yahoo.com>
2024-05-29 10:54:51 +0300

    add js script for bootstrap to head

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
 // Scripts and Styles
   function edutheme_files() {
     wp_enqueue_style('edutheme_google_font_montserrat', '//fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
-    wp_enqueue_script('jquery'); # Loading the WordPress bundled jQuery version.
-    wp_enqueue_style('edutheme_bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+    wp_enqueue_style('edutheme_bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
     wp_enqueue_style('edutheme_main_styles', get_theme_file_uri('/css/index.css'));
     wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
     wp_enqueue_script('main-js-file', get_theme_file_uri("/js/index.js"), array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'bootstrap-js', '//cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array('jquery'), true); // all the bootstrap javascript goodness
   }
 
   add_action('wp_enqueue_scripts', 'edutheme_files');
