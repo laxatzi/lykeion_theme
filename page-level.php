61b93ae2687ca8a59b73e12187327471db93c366
laxatzi <laxatzi@yahoo.com>
2024-05-30 10:18:48 +0300

    init page

---
+++ page-level.php
@@ -0,0 +1,19 @@
+<?php
+
+    get_header('inner');
+    get_template_part('template-parts/banners/page-banner');
+?>
+<!-- code here -->
+  <main class="container-fluid" id="section-main">
+    <div class="py-5" id="pricing-section">
+      <div class="container py-5">
+        <div class="row mt-md-5 mt-sm-4 mt-3">
+        </div>
+        <?php echo 'this is the level test page!'; ?>
+      </div>
+    </div>
+</mail>
+<?php
+    get_footer();
+
+?>
