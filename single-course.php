522b7ddce411f9b81ad205f21de6acf202dc96a9
laxatzi <laxatzi@yahoo.com>
2024-06-04 17:31:09 +0300

    code the loop:

--- single-course.php
+++ single-course.php
@@ -12,6 +12,26 @@
             <?php echo  get_the_post_thumbnail(null, array(1400, 800));  ?>
           </div>
         </div>
+         <div class="row">
+          <div class="course-info-main p-sm-4 p-3 mt-md-5 mt-sm-4 mt-3 ps-0 d-flex">
+            <div class="main p-sm-3 p-2 mb-md-0 mb-5">
+              <div class="course-title">
+                  <h1 class="fw-bold mt-4 mb-5"><?php the_title();?></h1>
+              </div>
+          <!-- main event content -->
+              <div id="intro-page">
+                <div class="main-event-content" id="intro">
+                  <?php
+                    the_content();
+                  ?>
+                </div>
+              </div>
+            </div>
+            <!-- end of course-info-main section -->
+        </div>
+<!-- end row -->
+      </div>
+<!-- end of container -->
       </div>
    </main>
 <?php
