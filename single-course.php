1b0d2f419576fc875fe87bc5e2b772354d8b16c9
laxatzi <laxatzi@yahoo.com>
2024-06-04 17:22:59 +0300

    add feature image

--- single-course.php
+++ single-course.php
@@ -1,15 +1,20 @@
 <?php
     get_header('inner');
     get_template_part('template-parts/banners/course-banner');
+        while(have_posts()) {
+    the_post();
+
   ?>
    <main id="virtual-classes" class="container-fluid courses">
       <div class="pt-5 container" >
         <div class="row mt-5">
-          <?php   echo 'this is the course page!';
- ?>
+          <div class="main-img">
+            <?php echo  get_the_post_thumbnail(null, array(1400, 800));  ?>
+          </div>
         </div>
       </div>
    </main>
 <?php
+        }
   get_footer();
 ?>
