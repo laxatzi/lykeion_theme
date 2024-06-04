f26e554da2c8802ca4876894b06415c1dfaae12b
laxatzi <laxatzi@yahoo.com>
2024-06-04 16:38:41 +0300

    create course page

---
+++ single-course.php
@@ -0,0 +1,15 @@
+<?php
+    get_header('inner');
+    get_template_part('template-parts/banners/event-banner');
+  ?>
+   <main id="virtual-classes" class="container-fluid courses">
+      <div class="pt-5 container" >
+        <div class="row mt-5">
+          <?php   echo 'this is the course page!';
+ ?>
+        </div>
+      </div>
+   </main>
+<?php
+  get_footer();
+?>
