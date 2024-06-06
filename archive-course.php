deb2f2128edc3df80eced03e82765501f890a33d
laxatzi <laxatzi@yahoo.com>
2024-06-06 18:37:14 +0300

    add excrept to posts

--- archive-course.php
+++ archive-course.php
@@ -73,9 +73,9 @@
 
                   $excerpt = get_the_excerpt();
 
-                  $excerpt = substr( $excerpt, 0, 140 ); // Only display first 260 characters of excerpt
+                  $excerpt = substr( $excerpt, 0, 140 ); // Only display first 140 characters of excerpt
                   $result = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
-                  echo $result;
+                  echo $excerpt;
 
                   ?>
                 </p>
