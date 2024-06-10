2e9bd2b87344f91c33153063bcff01b8305cb2a6
laxatzi <laxatzi@yahoo.com>
2024-06-10 10:30:00 +0300

    adjust queries

--- functions.php
+++ functions.php
@@ -148,3 +148,30 @@ function wp_display_popular_posts($num_posts) {
 
 add_theme_support( 'post-thumbnails' );
 
+function university_adjust_queries($query) {
+
+		if(!is_admin() AND is_post_type_archive('course') AND $query->is_main_query()) {
+
+			$query->set('orderby', 'title');
+			$query->set('order', 'ASC');
+			$query->set('posts_per_page', -1);
+		}
+
+
+		if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+			$today = date('Ymd');
+			$query->set('meta_key', 'event_date');
+			$query->set('oderby', 'meta_value_num');
+			$query->set('order', 'ASC');
+			$query->set('meta_query', array(
+					array(
+						'key' => 'event_date',
+						'compare' => '>=',
+						'value' => $today,
+						'type' => 'numeric'
+					)
+				));
+		}
+	}
+
+	add_action('pre_get_posts', 'university_adjust_queries');
