0b4baa9117665bbf4cd3f7570bdf1d2f45c4a403
laxatzi <laxatzi@yahoo.com>
2024-06-12 10:35:40 +0300

    add card for related course

--- single-course.php
+++ single-course.php
@@ -181,7 +181,90 @@
                  foreach($related_courses as $related_course) {
 
                   ?>
-                   <a href="<?php echo esc_url( get_permalink($related_course->ID)) ?>"><h5 class="fw-bold mb-2 mt-2" style="font-size: 1.15rem"><?php echo get_the_title($related_course); ?></h5></a>
+
+        <!-- start card display  -->
+            <div class="card p-3 me-3">
+              <div class="card-img-container">
+                <a href="<?php echo esc_url( get_permalink($related_course->ID)); ?>"><?php echo get_the_post_thumbnail(null, array( 400, 400)); ?></a>
+              </div>
+
+              <div class="card-body">
+                <div class="ratings d-flex mb-4">
+                  <div class="rating-stars d-flex">
+                    <span class="vicon-star"></span>
+                    <span class="vicon-star"></span>
+                    <span class="vicon-star"></span>
+                    <span class="vicon-star"></span>
+                    <span class="vicon-star"></span>
+
+                    <span class="votes">&#40;31&#41; </span>
+                  </div>
+                  <button class="heart">
+                    <span class="vicon-heart"></span>
+                  </button>
+                </div>
+                <div class="info-list d-flex mt-3 mb-3">
+                  <div class="info-list-level fw-bold d-flex">
+                    <img
+                      class="me-2 img-fluid"
+                      src="<?php echo get_theme_file_uri('img/icons/level-chart-thumb-16.png') ?>"
+                      alt=""
+                    />
+                    <span><?php the_field('course_level'); ?></span>
+                  </div>
+                  <div class="info-list-duration fw-bold d-flex">
+                    <span class="vicon-duration me-2"></span>
+                    <span><?php the_field('course_duration'); ?> days</span>
+                  </div>
+                </div>
+                 <a href="<?php echo esc_url( get_permalink($related_course->ID)) ?>"><h5 class="fw-bold mb-2 mt-2" style="font-size: 1.15rem"><?php echo get_the_title($related_course); ?></h5></a>
+                <p class="card-text">
+                <?php
+
+                  $excerpt = get_the_excerpt();
+
+                  $excerpt = substr( $excerpt, 0, 120 ); // Only display first 140 characters of excerpt
+                  $result = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
+                  echo $result." [&hellip;]";
+
+                  ?>
+                </p>
+                <div
+                  class="tutor d-sm-flex flex-column align-items-start justify-content-center mt-4"
+                >
+                  <div class="tutor-profile d-flex">
+                    <img
+                      class="img-fluid me-1"
+                      src="<?php the_field('author_image') ?>"
+                      alt="teacher's profile"
+                    />
+                    <h4 class="fw-bold pt-2"><?php the_field('course_instructor'); ?></h4>
+                  </div>
+                  <div class="tutor-lessons d-flex mt-2">
+                    <div class="lesson-icon me-2">
+                      <span class="vicon-lesson"></span>
+                    </div>
+                    <div class="lesson-number d-flex">
+                      <span><?php the_field('course_lessons'); ?></span>&nbsp;
+                      <p>Lessons</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="price-list d-flex mt-5">
+                  <div class="price price-current">$<?php the_field('course_price'); ?></div>
+                  <div class="price price-prev">
+                    <s class="ms-4">$<?php the_field('course_old_price'); ?></s>
+                  </div>
+                </div>
+
+                <div class="read-more mt-3 mb-3 btn btn-lg">
+                  <a href="<?php echo esc_url( get_permalink($related_course->ID)) ?>" class="link-btn d-flex"
+                    >Enroll now <span class="vicon-arrow--right"></span
+                  ></a>
+                </div>
+              </div>
+            </div>
+            <!-- end card -->
                    <?php
                  }
                 } else {
