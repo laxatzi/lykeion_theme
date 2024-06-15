825315cf8cf6c1e250e75b5552b177a56778a196
laxatzi <laxatzi@yahoo.com>
2024-06-15 11:55:31 +0300

    remove iframe

--- page-contact.php
+++ page-contact.php
@@ -172,13 +172,7 @@
       <div class="container">
         <div class="row my-lg-5">
           <div class="my-5 d-flex justify-content-center align-items-center">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115681.29592731265!2d-77.47713270775661!3d25.0326996781907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x892f7c99b981dbc9%3A0x2aef01d3485e50d2!2sNassau%2C%20Bahamy!5e0!3m2!1spl!2spl!4v1624445118063!5m2!1spl!2spl"
-              class="w-100 my-4"
-              height="400"
-              allowfullscreen=""
-              loading="lazy"
-            ></iframe>
+
           </div>
         </div>
       </div>
