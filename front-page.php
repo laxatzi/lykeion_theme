b108c66f117792a0a42ca75427f704ed07101f94
laxatzi <laxatzi@yahoo.com>
2024-09-23 13:03:08 +0300

    add custom fields

--- front-page.php
+++ front-page.php
@@ -301,11 +301,7 @@ get_header();
                  <a href="<?php  echo get_the_permalink(); ?>">
                         <?php  echo get_the_post_thumbnail(null, array( 400, 500));  ?>
                       </a>
-                <!-- <img
-                  src="<?php echo get_theme_file_uri('/img/restaurant-workers.jpg') ?>"
-                  class="card-img-top img-fluid"
-                  alt=""
-                /> -->
+
               </div>
 
               <div class="card-body">
@@ -330,11 +326,11 @@ get_header();
                       src="<?php echo get_theme_file_uri('/img/icons/level-chart-thumb-16.png') ?>"
                       alt=""
                     />
-                    <span>Beginner</span>
+                    <span><?php the_field('course_level') ?></span>
                   </div>
                   <div class="info-list-duration fw-bold d-flex">
                     <span class="vicon-duration me-2"></span>
-                    <span>3h 34m</span>
+                    <span><?php the_field('course_duration') ?> days</span>
                   </div>
                 </div>
                  <a href="<?php  echo get_the_permalink(); ?>">
@@ -350,12 +346,11 @@ get_header();
                   class="tutor d-sm-flex flex-column align-items-start justify-content-center mt-4"
                 >
                   <div class="tutor-profile d-flex">
-                    <img
-                      class="img-fluid me-1"
-                      src="<?php echo get_theme_file_uri('/img/thumbs/sigmund-thumb.jpg') ?>"
-                      alt="teacher's profile"
-                    />
-                    <h4 class="fw-bold pt-2">Daniel Gerard</h4>
+
+
+              <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+
+                    <h4 class="fw-bold pt-2 ps-2"><?php  the_author_posts_link(); ?></h4>
                   </div>
                   <div class="tutor-lessons d-flex mt-2">
                     <div class="lesson-icon me-1">
@@ -368,9 +363,9 @@ get_header();
                   </div>
                 </div>
                 <div class="price-list d-flex mt-5">
-                  <div class="price price-current">$55.99</div>
+                  <div class="price price-current">$<?php the_field('course_price') ?></div>
                   <div class="price price-prev">
-                    <s class="ms-4"> $69.99</s>
+                    <s class="ms-2"> $<?php the_field('course_old_price') ?></s>
                   </div>
                 </div>
 
