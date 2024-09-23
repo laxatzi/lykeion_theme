28a2add3b2f27046bcad83339adc665173d57a53
laxatzi <laxatzi@yahoo.com>
2024-09-23 17:04:30 +0300

    clean archive tutor code

---
+++ archive-tutor.php
@@ -0,0 +1,45 @@
+<?php
+   get_header('inner');
+    get_template_part('template-parts/banners/archive-banner');
+?>
+   <main class="container-fluid">
+
+      <div class="my-5 container" >
+         <h2>Meet our tutors: </h2>
+        <div class="row mt-5">
+           <div class="p-3 col-lg-10 col-md-10">
+
+          <?php
+            while(have_posts()) {
+              the_post();
+            ?>
+            <div class="the-blogPost"  >
+           <div class="blogPost-title p-4 mb-4">
+            <a href="<?php the_permalink(); ?>">
+              <h2 class="fw-bold"><?php the_title(); ?></h2>
+              </a>
+              <?php the_content(); ?>
+
+           </div>
+
+          </div>
+          <?php
+            }
+          ?>
+          </div>
+         <nav>
+            <div class="events-pagination d-flex gap-1 container">
+                <?php the_posts_pagination( array(
+                    'mid_size'  => 5,
+                    'prev_text' => __( '<', 'textdomain' ),
+                    'next_text' => __( '>', 'textdomain' ),
+                ) ); ?>
+            </div>
+          </nav>
+
+        </div>
+      </div>
+   </main>
+<?php
+  get_footer();
+?>
