9ee9812aab37a936c21ca4ec86f7c2c06cce492e
laxatzi <laxatzi@yahoo.com>
2024-09-25 17:19:34 +0300

    create new template part

---
+++ template-parts/banners/shape-banner.php
@@ -0,0 +1,30 @@
+
+      <!-- Shapes -->
+      <div class="shapes">
+        <div class="shape-image shape-image-32">
+          <img
+            class="img-fluid"
+            src="<?php echo get_theme_file_uri('/img/icons/world_orange-small.png') ?>"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-33">
+          <img
+            class="img-fluid"
+            src="<?php echo get_theme_file_uri('/img/shapes/shape-circles.png') ?>"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-34">
+          <img
+            class="img-fluid"
+            src="<?php echo get_theme_file_uri('/img/shapes/geo-shape-transparent-15.png') ?>"
+            alt=""
+          />
+        </div>
+        <div class="shape-image shape-image-35">
+          <img class="img-fluid"
+          src="<?php echo get_theme_file_uri('/img/icons/abstract419.png') ?>"
+          alt="" />
+        </div>
+      </div>
