1ddd32531e139955446b300277304c2560a4e137
laxatzi <laxatzi@yahoo.com>
2024-09-25 17:30:56 +0300

    remove dup code

--- template-parts/banners/post-banner.php
+++ template-parts/banners/post-banner.php
@@ -20,32 +20,3 @@
           </nav>
         </div>
       </section>
-      <!-- Shapes -->
-      <div class="shapes">
-        <div class="shape-image shape-image-32">
-          <img
-            class="img-fluid"
-            src="<?php echo get_theme_file_uri('/img/icons/world_orange-small.png') ?>"
-            alt=""
-          />
-        </div>
-        <div class="shape-image shape-image-33">
-          <img
-            class="img-fluid"
-            src="<?php echo get_theme_file_uri('/img/shapes/shape-circles.png') ?>"
-            alt=""
-          />
-        </div>
-        <div class="shape-image shape-image-34">
-          <img
-            class="img-fluid"
-            src="<?php echo get_theme_file_uri('/img/shapes/geo-shape-transparent-15.png') ?>"
-            alt=""
-          />
-        </div>
-        <div class="shape-image shape-image-35">
-          <img class="img-fluid"
-          src="<?php echo get_theme_file_uri('/img/icons/abstract419.png') ?>"
-          alt="" />
-        </div>
-      </div>
