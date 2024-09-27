302150f407d847614a0fa64294f80abad28dca98
laxatzi <laxatzi@yahoo.com>
2024-09-27 13:20:06 +0300

    replace span vicon with i font-awsome

--- footer.php
+++ footer.php
@@ -132,7 +132,7 @@
     <div class="search-overlay">
       <div class="search-overlay__top">
         <div class="container">
-          <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+          <span class="vicon-magnifying-glass" aria-hidden="true"></span>
           <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
           <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
         </div>
