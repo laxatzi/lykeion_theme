7c475b492a18a901a96c1be3c8f576217eedda88
laxatzi <laxatzi@yahoo.com>
2024-10-01 15:39:10 +0300

    add script to theme files

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
     wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
     wp_enqueue_style('edutheme_search_styles', get_theme_file_uri('/css/search.css'));
     wp_enqueue_script('main-js-file', get_theme_file_uri("/js/index.js"), array('jquery'), '1.0.0', true );
-    // wp_enqueue_script('search-js-file', get_theme_file_uri("/js/searchin.js"), array('jquery'), '1.1.0', true );
+    wp_enqueue_script('search-js-file', get_theme_file_uri("/js/searchin.js"), array('jquery'), '1.1.0', true );
     wp_enqueue_script( 'bootstrap-js', '//cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array('jquery'), true); // all the bootstrap javascript goodness
   }
 
@@ -53,11 +53,11 @@
 
     }
 
-    if ( is_page('search') ) {
+    // if ( is_page('search') ) {
 
-    wp_enqueue_script('my-custom-script', get_stylesheet_directory_uri() . '/js/searchin.js', array('jquery'), false, true);
+    // wp_enqueue_script('my-custom-script', get_stylesheet_directory_uri() . '/js/searchin.js', array('jquery'), false, true);
 
-    }
+    // }
 
 }
 
