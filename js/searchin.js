dbf45fed5bd33237d1596e59c8c591e5135be506
laxatzi <laxatzi@yahoo.com>
2024-10-01 15:38:28 +0300

    convert jquery code to vanilla js

--- js/searchin.js
+++ js/searchin.js
@@ -1,25 +1,64 @@
-import $ from "jquery";
-
 class Search {
   // 1. describe and create/initiate our object
   constructor() {
-    this.openButton = $(".js-search-trigger");
-    this.closeButton = $(".search-overlay__close");
-    this.searchOverlay = $(".search-overlay");
-    this.isOverlayOpen = false;
+    this.resultsDiv = document.querySelector("#search-overlay__results");
+    this.openButton = document.querySelectorAll(".js--search-trigger");
+    this.closeButton = document.querySelector(".search-overlay__close");
+    this.searchOverlay = document.querySelector(".search-overlay");
+    this.searchField = document.querySelector("#search-term");
     this.events();
+    this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
+    this.typingTimer;
   }
 
   // 2. events
   events() {
-    this.openButton.on("click", this.openOverlay.bind(this));
-    this.closeButton.on("click", this.closeOverlay.bind(this));
-    $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.openButton.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.openOverlay();
+      });
+    });
+
+    this.closeButton.addEventListener("click", () => this.closeOverlay());
+    document.addEventListener("keydown", (e) => this.keyPressDispatcher(e));
+    this.searchField.addEventListener("keyup", () => this.typingLogic());
   }
 
   // 3. methods (function, action...)
+  typingLogic() {
+    if (this.searchField.value != this.previousValue) {
+      clearTimeout(this.typingTimer);
+
+      if (this.searchField.value) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.innerHTML = '<div class="spinner-loader"></div>';
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750);
+      } else {
+        this.resultsDiv.innerHTML = "";
+        this.isSpinnerVisible = false;
+      }
+    }
+
+    this.previousValue = this.searchField.value;
+  }
+
+  getResults() {
+    his.resultsDiv.innerHTML = "Imagin real search results here...";
+    this.isSpinnerVisible = false;
+  }
+
   keyPressDispatcher(e) {
-    if (e.keyCode == 83 && !this.isOverlayOpen) {
+    if (
+      e.keyCode == 83 &&
+      !this.isOverlayOpen &&
+      document.activeElement.tagName != "INPUT" &&
+      document.activeElement.tagName != "TEXTAREA"
+    ) {
       this.openOverlay();
     }
 
@@ -29,18 +68,19 @@ class Search {
   }
 
   openOverlay() {
-    this.searchOverlay.addClass("search-overlay--active");
-    $("body").addClass("body-no-scroll");
-    console.log("our open method just ran");
+    this.searchOverlay.classList.add("search-overlay--active");
+    document.body.classList.add("body-no-scroll");
+    this.searchField.value = "";
+    setTimeout(() => this.searchField.focus(), 301);
+    console.log("our open method just ran!");
     this.isOverlayOpen = true;
+    return false;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
-    console.log("our close method just ran");
+    console.log("our close method just ran!");
     this.isOverlayOpen = false;
   }
 }
-
-const search = new Search();
