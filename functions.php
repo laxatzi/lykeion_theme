ad49d2385051902e16a434ec103dc044023d883d
laxatzi <laxatzi@yahoo.com>
2024-10-02 12:36:54 +0300

    remove searchin script

--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@
     wp_enqueue_style('edutheme_vicon_styles', get_theme_file_uri('/css/vicons.css'));
     wp_enqueue_style('edutheme_search_styles', get_theme_file_uri('/css/search.css'));
     wp_enqueue_script('main-js-file', get_theme_file_uri("/js/index.js"), array('jquery'), '1.0.0', true );
-    wp_enqueue_script('search-js-file', get_theme_file_uri("/js/searchin.js"), array('jquery'), '1.1.0', true );
     wp_enqueue_script( 'bootstrap-js', '//cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array('jquery'), true); // all the bootstrap javascript goodness
   }
 
