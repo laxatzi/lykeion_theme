9e866615bd850d323070774534e4d7207d326371
laxatzi <laxatzi@yahoo.com>
2024-10-24 15:59:58 +0300

    rename register_rest_route route param

--- includes/searchQuery-route.php
+++ includes/searchQuery-route.php
@@ -2,7 +2,7 @@
   add_action('rest_api_init', 'eduthemeRegisterSearch');
 
   function eduthemeRegisterSearch() {
-    register_rest_route('edutheme/v1', 'restQueries', array(
+    register_rest_route('edutheme/v1', 'searchQuery', array(
       'methods' => WP_REST_Server::READABLE,
       'callback' => 'eduthemeCustomRoute'
     ) );
