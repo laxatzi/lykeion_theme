5f4dc78ae108edfbb0474c5a0f2125d3d0b9bd65
laxatzi <laxatzi@yahoo.com>
2024-11-05 16:38:51 +0200

    create search form

---
+++ searchform.php
@@ -0,0 +1,6 @@
+                <form class="search-form" method="get" action="<?php echo esc_url(site_url('')) ?>">
+                  <input type="search" name="s" class="form-control p-2" aria-label="Search keyword">
+                  <div class="input-group-append">
+                    <input class="input-group-search p-2 fw-bold" type="submit" value="Search">
+                  </div>
+                </form>
