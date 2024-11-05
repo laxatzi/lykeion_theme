81a0530c63c1e1943b11d63c92732e4630fcaa4a
laxatzi <laxatzi@yahoo.com>
2024-11-05 18:28:00 +0200

    refactor- add searchform replacing form

--- page-search.php
+++ page-search.php
@@ -12,12 +12,7 @@
           <div class="row mt-sm-5">
             <h2><?php the_title(); ?></h2>
              <div class="input-group mb-3 d-flex align-items-center">
-                <form class="search-form" method="get" action="<?php echo esc_url(site_url('')) ?>">
-                  <input type="search" name="s" class="form-control p-2" aria-label="Search keyword" placeholder="What are you looking for?">
-                  <div class="input-group-append">
-                    <input class="input-group-search p-2 fw-bold" type="submit" value="Search">
-                  </div>
-                </form>
+                  <?php get_search_form(); ?>
               </div>
           </div>
        </div>
